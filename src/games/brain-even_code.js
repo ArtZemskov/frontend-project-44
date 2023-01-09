dd193b5080709ee00e8c874e0932879d1ecfb848
Артем Земсков <artem_zemskov@MacBook-Air.local>
2023-01-09 10:06:34 +0100

    Created /games

---
+++ src/games/brain-even_code.js
@@ -0,0 +1,16 @@
+import startGame from '../src/index.js';
+import getRandomNumber from '../src/randomNumber.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (num) => num % 2 === 0;
+
+const gameLogic = () => {
+  const question = getRandomNumber(0, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const runEven = () => {
+  startGame(description, gameLogic);
+};
+export default runEven;
