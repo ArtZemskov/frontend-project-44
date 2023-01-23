116fa0354f72da66cd6dd30d03f9eb579232494d
Артем Земсков <artem_zemskov@MacBook-Air.local>
2023-01-23 15:10:18 +0100

    Added game logic

---
+++ src/games/brain-progression_code.js
@@ -0,0 +1,30 @@
+import startGame from '../index.js';
+import getRandomNumber from '../randomNumber.js';
+
+const description = 'What number is missing in the progression?';
+
+const getProgression = (firstNum, step) => {
+  const progression = [];
+  const progressionLength = 10;
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(firstNum + step * i);
+  }
+  return progression;
+};
+
+const gameLogic = () => {
+  const firstNum = getRandomNumber(0, 50);
+  const step = getRandomNumber(2, 10);
+  const progression = getProgression(firstNum, step);
+  const randomSpace = getRandomNumber(1, 8);
+  const correctAnswer = String(progression[randomSpace]);
+  progression[randomSpace] = '..';
+  const question = progression.join(' ');
+  
+  return [question, correctAnswer];
+};
+
+const runProgression = () => {
+  startGame(description, gameLogic);
+};
+export default runProgression;
