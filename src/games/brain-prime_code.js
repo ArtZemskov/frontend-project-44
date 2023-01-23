1336f82728122cb25606000497e9e8d6193647b8
Артем Земсков <artem_zemskov@MacBook-Air.local>
2023-01-23 17:50:54 +0100

    Created brain-prime game logic

---
+++ src/games/brain-prime_code.js
@@ -0,0 +1,23 @@
+import startGame from '../index.js';
+import getRandomNumber from '../randomNumber.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const gameLogic = () => {
+  const question = getRandomNumber(0, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const runPrime = () => {
+  startGame(description, gameLogic);
+};
+export default runPrime;
