e799133cf983fa318ef73abb192c710d7f0693f8
Артем Земсков <artem_zemskov@MacBook-Air.local>
2023-01-30 14:33:44 +0100

    const gcd renamed as getGcd

--- src/games/brain-gcd_code.js
+++ src/games/brain-gcd_code.js
@@ -3,11 +3,11 @@ import getRandomNumber from '../randomNumber.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const gcd = (num1, num2) => {
+const getGcd = (num1, num2) => {
   let num3;
   if (num2 !== 0) {
     num3 = num1 % num2;
-    return gcd(num2, num3);
+    return getGcd(num2, num3);
   }
   return num1;
 };
@@ -16,7 +16,7 @@ const getQuestionAndAnswer = () => {
   const num1 = getRandomNumber(0, 100);
   const num2 = getRandomNumber(0, 50);
   const question = `${num1} ${num2}`;
-  const correctAnswer = String(gcd(num1, num2));
+  const correctAnswer = String(getGcd(num1, num2));
   return [question, correctAnswer];
 };
 
