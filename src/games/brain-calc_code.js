eeb8ab6e5a7272f0610d16278a49e7f48a1b3fdd
Артем Земсков <artem_zemskov@MacBook-Air.local>
2023-01-30 14:27:07 +0100

    fix

--- src/games/brain-calc_code.js
+++ src/games/brain-calc_code.js
@@ -4,24 +4,24 @@ import getRandomNumber from '../randomNumber.js';
 const description = 'What is the result of the expression?';
 const collOfOperators = ['+', '-', '*'];
 
+const calculate = (operator, x, y) => {
+  switch (operator) {
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
+    default:
+      return null;
+  }
+};
+
 const getQuestionAndAnswer = () => {
   const randomOperator = collOfOperators[Math.floor(Math.random() * collOfOperators.length)];
   const num1 = getRandomNumber(0, 25);
   const num2 = getRandomNumber(0, 25);
   const question = `${num1} ${randomOperator} ${num2}`;
-  const calculate = (operator, x, y) => {
-    switch (operator) {
-      case '+':
-        return x + y;
-      case '-':
-        return x - y;
-      case '*':
-        return x * y;
-      default:
-        return null;
-    }
-  };
-
   const correctAnswer = String(calculate(randomOperator, num1, num2));
   return [question, correctAnswer];
 };
